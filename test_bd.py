83e482f5a02bb39579f5b7bd7db4887d935967ba
irnef <ikosharik1999@gmail.com>
2021-07-25 14:18:30 +0300

    Смена бд на sqlite

--- test_bd.py
+++ test_bd.py
@@ -1,129 +1,163 @@
 import difflib
-from datetime import datetime, strftime
+import sqlite3 as sq
+from datetime import datetime
 
-from sqlalchemy import create_engine, MetaData, Table, text
+import sqlalchemy as sa
+# server = 'IRNEF\SQLEXPRESS'
+# database_testBD = 'test_bd'
+# database_BMS = 'BMS'
 
 
-server = 'IRNEF\SQLEXPRESS'
-database_testBD = 'test_bd'
-database_BMS = 'BMS'
-
-
-def makeEngine(server, database):
-    engine = create_engine(
-        'mssql+pyodbc://irnef:12345678@' + server + '/' + database + '?driver=ODBC+Driver+17+for+SQL+Server')
+def makeEngine():
+    try:
+        engine = sa.create_engine('sqlite:///prob_bd.db')
+        print('Engine OK')
+    except:
+        print('Engine Error')
     return engine
 
 
-def makeConnection(engine):
+def makeConnection():
     try:
-        conn = engine.connect()
+        conn = sq.connect('prob_bd.db')
         print("Connection done")
-    except ConnectionError as e:
-        print("The error '{e}' occurred")
+        # print("The error '{e}' occurred")
+    except:
+        print("Error connection")
+        # print("The error '{e}' occurred")
     return conn
 
 
+def createCurs(conn):
+    crs = conn.cursor()
+    crs.execute('''select * from Users''')
+    print(crs.fetchall())
+    conn.commit()
+
+
 def makeMeta(engine):
-    metadata = MetaData()
-    metadata.reflect(engine)
-    return metadata
-
-
-def getScripts(engine):
-    metadata_testBD = makeMeta(engine_testBD)
+    meta = sa.MetaData()
+    meta.reflect(engine)
+    print(meta.tables)
+    for i in meta.tables:
+        print(i)
+    print(dict.values(meta.tables))
     tables_n = []  # имена таблиц
     tables = []  # объекты таблиц
+    keys = meta.tables.keys()  # получение наименований таблиц
+    tables_n = [key for key in keys]  # запись наименований таблиц в список
     fileName = datetime.strftime(datetime.now(), '%Y-%m-%dt%H-%M-%S.txt')
-    print(fileName)
     file = open(fileName, "w")
-    keys = metadata_testBD.tables.keys()  # получение наименований таблиц
-    tables_n = [key for key in keys] # запись наименований таблиц в список
-    # print(tables_n) # печать наименований всех таблиц
-    # for i in metadata.tables:
+    print(fileName)
+    print(tables_n)
     for n in range(len(tables_n)):
         name_t = tables_n[n]
-        table = Table(name_t, metadata_testBD, autoload=True, autoload_with=engine)
-        # print('Имя таблицы: ', tables_n[n])  # вывод информации о всех колонках каждой таблицы
-        file.write('Имя таблицы: ' + tables_n[n] + '\n')
+        table = sa.Table(name_t, meta, autoload=True, autoload_with=engine)
+        print('TableName: ', tables_n[n])  # вывод информации о всех колонках каждой таблицы
+        file.write('TableName: ' + tables_n[n] + '\n')
         for j in table.columns:
-            #   print(j.name, j.type)
+            print(j.name, j.type)
             file.write(str(j.name) + ', type: ' + str(j.type) + '\n')
 
     file.close()
     return file
-    # print()
 
+
+# def getScripts(engine):
+#     metadata_testBD = makeMeta(engine_testBD)
+#     tables_n = []  # имена таблиц
+#     tables = []  # объекты таблиц
+#     fileName = datetime.strftime(datetime.now(), '%Y-%m-%dt%H-%M-%S.txt')
+#     print(fileName)
+#     file = open(fileName, "w")
+#     keys = metadata_testBD.tables.keys()  # получение наименований таблиц
+#     tables_n = [key for key in keys]  # запись наименований таблиц в список
+#     # print(tables_n) # печать наименований всех таблиц
+#     # for i in metadata.tables:
+#     for n in range(len(tables_n)):
+#         name_t = tables_n[n]
+#         table = Table(name_t, metadata_testBD, autoload=True, autoload_with=engine)
+#         # print('Имя таблицы: ', tables_n[n])  # вывод информации о всех колонках каждой таблицы
+#         file.write('Имя таблицы: ' + tables_n[n] + '\n')
+#         for j in table.columns:
+#             #   print(j.name, j.type)
+#             file.write(str(j.name) + ', type: ' + str(j.type) + '\n')
+#
+#     file.close()
+#     return file
+#     # print()
 
 tableName = 'Users'
 tableColumns = ' (surName, name, midName, isHead, departmentId, email, phoneNumber, pass, userIdBot) '
 values1 = " ('Демидов', 'Артем', 'Андреевич', 1, 1, '123','123','123') "
 
-
-def insData(tableName, tableColumns, values):
-    # del values[4]
-    values[3] = int(values[3])
-    values[4] = int(values[4])
-    engine_BMS = makeEngine(server, database_BMS)
-    conn_BMS = makeConnection(engine_BMS)
-    # metadata_BMS = makeMeta(engine_BMS)
-
-    # getScripts(metadata_BMS, engine_BMS)
-
-    # cursor = conn.cursor()
-    # t = text("select * from Users")
-    try:
-        test = "insert into " + tableName + tableColumns + " values ('" + str(
-        values[0]) + "', '" + str(values[1]) + "', '" + str(
-        values[2]) + "', " + str(values[3]) + ", " + str(values[4]) + ", '" + str(values[5]) + "', '" + str(
-        values[6]) + "','" + str(values[7]) + "','" + str(values[8]) + "')"
-
-        t = text(test)
-        result = conn_BMS.execute(t)
-        return result
-    except Exception:
-        print('Ошибка ввода данных')
-        return 'Error'
-    # print(result.fetchall()) # отрабатывает при запуске select
-    # conn.commit() # connection object has no attribute 'commit', но без него данные пишутся
-
+# def insData(tableName, tableColumns, values):
+#     # del values[4]
+#     values[3] = int(values[3])
+#     values[4] = int(values[4])
+#     engine_BMS = makeEngine(server, database_BMS)
+#     conn_BMS = makeConnection(engine_BMS)
+#     # metadata_BMS = makeMeta(engine_BMS)
+#
+#     # getScripts(metadata_BMS, engine_BMS)
+#
+#     # cursor = conn.cursor()
+#     # t = text("select * from Users")
+#     try:
+#         test = "insert into " + tableName + tableColumns + " values ('" + str(
+#             values[0]) + "', '" + str(values[1]) + "', '" + str(
+#             values[2]) + "', " + str(values[3]) + ", " + str(values[4]) + ", '" + str(values[5]) + "', '" + str(
+#             values[6]) + "','" + str(values[7]) + "','" + str(values[8]) + "')"
+#
+#         t = text(test)
+#         result = conn_BMS.execute(t)
+#         return result
+#     except Exception:
+#         print('Ошибка ввода данных')
+#         return 'Error'
+#     # print(result.fetchall()) # отрабатывает при запуске select
+#     # conn.commit() # connection object has no attribute 'commit', но без него данные пишутся
 
 
-def getData(depsId):
-    print(depsId)
-    departIdUser = []
-    engine_BMS1 = makeEngine(server, database_BMS)
-    conn_BMS1 = makeConnection(engine_BMS1)
-    for i, dep in enumerate(depsId):
-        dep_s = ''.join(depsId[i])
-        t = text("select departmentId, userIdBot from Users where departmentId = " + dep_s)
-        result = conn_BMS1.execute(t)
-        for j in result:
-            departIdUser.append(j)
-    return departIdUser
+# def getData(depsId):
+#     print(depsId)
+#     departIdUser = []
+#     engine_BMS1 = makeEngine(server, database_BMS)
+#     conn_BMS1 = makeConnection(engine_BMS1)
+#     for i, dep in enumerate(depsId):
+#         dep_s = ''.join(depsId[i])
+#         t = text("select departmentId, userIdBot from Users where departmentId = " + dep_s)
+#         result = conn_BMS1.execute(t)
+#         for j in result:
+#             departIdUser.append(j)
+#     return departIdUser
 
-    # print(result.fetchall())  # отрабатывает при запуске select
+# print(result.fetchall())  # отрабатывает при запуске select
 
-#def checkPas():
 
+# def checkPas():
 
 
 def compareDiff(file1, file2):
     diff = difflib.ndiff(open(file1).readlines(), open(file2).readlines())
-    # print(*diff)
-    # my_file = Path("C:/Users/user/Documents/file3.txt")
+#     # print(*diff)
+#     # my_file = Path("C:/Users/user/Documents/file3.txt")
     file = open('file3.txt', 'w')
     file1 = open('test.txt', 'w')
     for i in diff:
         file.write(i)
         file1.write(i)
-    # if my_file.exists():
-    #     print('нашел')
-    #     tbot.sendMes(392812944, 'привет')
+#     # if my_file.exists():
+#     #     print('нашел')
+#     #     tbot.sendMes(392812944, 'привет')
 
 
-engine_testBD = makeEngine(server, database_testBD)  # создание объекта бд, с которой нужно получать скрипты
-conn_testBD = makeConnection(engine_testBD)
+# engine_testBD = makeEngine(server, database_testBD)  # создание объекта бд, с которой нужно получать скрипты
+conn_testBD = makeConnection()
+createCurs(conn_testBD)
+engineLite = makeEngine()
+makeMeta(engineLite)
+compareDiff('2021-04-23t17-30-43.txt', '2021-07-25t13-31-03.txt')
 
 # c = metadata.tables[j]
 # for i in c:
